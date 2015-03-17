8042807e393554daf12a206ea030915fa821c0d2
Gert Pellin <gert@gepe-biljarts.be>
2015-03-17 12:05:47 +0100

    1 VERKEERSLICHT (HOOFDSTUK 1 – 2)

--- Verkeerslicht/MainWindow.xaml.cs
+++ Verkeerslicht/MainWindow.xaml.cs
@@ -24,5 +24,37 @@ namespace Verkeerslicht
         {
             InitializeComponent();
         }
+
+        private void ButtonGo_Click(object sender, RoutedEventArgs e)
+        {
+            GroenLicht.Opacity = 1;
+            OranjeLicht.Opacity = 0;
+            ButtonOpgelet.IsEnabled = true;
+            ButtonGo.IsEnabled = false;
+        }
+
+        private void ButtonStop_Click(object sender, RoutedEventArgs e)
+        {
+            OranjeLicht.Opacity = 0;
+            RoodLicht.Opacity = 1;
+            ButtonOpgelet.IsEnabled = true;
+            ButtonStop.IsEnabled = false;
+        }
+
+        private void ButtonOpgelet_Click(object sender, RoutedEventArgs e)
+        {
+            if (GroenLicht.Opacity == 1)
+            {
+                ButtonStop.IsEnabled = true;
+                GroenLicht.Opacity = 0;
+            }
+            else
+            {
+                ButtonGo.IsEnabled = true;
+                RoodLicht.Opacity = 0;
+            }
+            OranjeLicht.Opacity = 1;
+            ButtonOpgelet.IsEnabled = false;
+        }
     }
 }
