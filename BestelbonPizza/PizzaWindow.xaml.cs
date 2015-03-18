4057592b9102ff66aa1bef25e315b1ca37b4a51b
Gert Pellin <gert@gepe-biljarts.be>
2015-03-18 13:29:34 +0100

    2 Bestelbon pizza (hoofdstuk 3 - 4)

--- BestelbonPizza/PizzaWindow.xaml.cs
+++ BestelbonPizza/PizzaWindow.xaml.cs
@@ -20,9 +20,46 @@ namespace BestelbonPizza
     /// </summary>
     public partial class PizzaWindow : Window
     {
+
+        public string maat = "medium";
+        public int AantalInt = 1;
         public PizzaWindow()
         {
+            
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            bestelling.Text = "U heeft "+AantalInt+" "+maat+ " pizza(\'s) besteld met: ";
+            foreach(CheckBox x in Beleg.Children)
+            {
+                if (x.IsChecked == true) bestelling.Text += x.Content+" ";
+            }
+            if (ExtraDikkeKorst.IsChecked == true) bestelling.Text += "met extra dikke korst ";
+            if (ExtraKaas.IsChecked == true) bestelling.Text += "met extra kaas ";
+
+        }
+
+
+        private void Maat_Checked(object sender, RoutedEventArgs e)
+        {
+            maat = (string)((RadioButton)sender).Content;
+        }
+
+        private void plus_Click(object sender, RoutedEventArgs e)
+        {
+            Aantal.Content=++AantalInt;
+            Min.IsEnabled = true;
+        }
+
+        private void min_Click(object sender, RoutedEventArgs e)
+        {
+            if (AantalInt > 1)
+            {
+                Aantal.Content = --AantalInt;
+                if (AantalInt == 1) ((Button)sender).IsEnabled = false;
+            }
         }
     }
 }
