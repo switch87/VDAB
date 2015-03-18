ff1cfe0f0b85632ef6573306c7c995995474fe55
Gert Pellin <gert@gepe-biljarts.be>
2015-03-18 16:04:36 +0100

    3 TELEFOON eerste gedeelte

---
+++ Telefoon/Persoon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace Telefoon
+{
+    public class Persoon
+    {
+        public string Naam { get; set; }
+        public string Telefoonnr { get; set; }
+        public string Groep { get; set; }
+
+    }
+}
