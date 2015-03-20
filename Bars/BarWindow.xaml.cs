ad40b85fe015e0e2d8ff1351680ef4e77fb51abe
Gert Pellin <gert@gepe-biljarts.be>
2015-03-20 11:10:20 +0100

    7.1.1 Werkwijze

--- Bars/BarWindow.xaml.cs
+++ Bars/BarWindow.xaml.cs
@@ -24,5 +24,51 @@ namespace Bars
         {
             InitializeComponent();
         }
+
+        private void Vet_Aan_Uit()
+        {
+            if (TextBoxVoorbeeld.FontWeight==FontWeights.Normal)
+            {
+                TextBoxVoorbeeld.FontWeight = FontWeights.Bold;
+                MenuVet.IsChecked = true;
+            }
+            else
+            {
+                TextBoxVoorbeeld.FontWeight = FontWeights.Normal;
+                MenuVet.IsChecked = false;
+            }
+        }
+        private void MenuVet_Click(object sender, RoutedEventArgs e)
+        {
+            Vet_Aan_Uit();
+        }
+        private void Schuin_Aan_Uit()
+        {
+            if (TextBoxVoorbeeld.FontStyle==FontStyles.Normal)
+            {
+                TextBoxVoorbeeld.FontStyle = FontStyles.Italic;
+                MenuSchuin.IsChecked = true;
+            }
+            else
+            {
+                TextBoxVoorbeeld.FontStyle = FontStyles.Normal;
+                MenuSchuin.IsChecked = false;
+            }
+        }
+        private void MenuSchuin_Click(object sender, RoutedEventArgs e)
+        {
+            Schuin_Aan_Uit();
+        }
+        private void Lettertype_Click(object sender, RoutedEventArgs e)
+        {
+            MenuItem hetLettertype = (MenuItem)sender;
+            foreach (MenuItem huidig in Fontjes.Items)
+            {
+                huidig.IsChecked = false;
+            }
+            hetLettertype.IsChecked = true;
+            TextBoxVoorbeeld.FontFamily = new FontFamily(hetLettertype.Header.ToString());
+        }
+
     }
 }
