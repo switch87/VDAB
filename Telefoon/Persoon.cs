c537ca3e0e86ec3012bb91eaed62dbb4dd88cfd6
Gert Pellin <gert@gepe-biljarts.be>
2015-03-20 10:13:28 +0100

    3 TELEFOON (HOOFDSTUK 5 - 6)

--- Telefoon/Persoon.cs
+++ Telefoon/Persoon.cs
@@ -12,6 +12,15 @@ namespace Telefoon
         public string Naam { get; set; }
         public string Telefoonnr { get; set; }
         public string Groep { get; set; }
+        public BitmapImage Foto { get; set; }
+
+        public Persoon(string naam, string telefoonnr, string groep, BitmapImage foto)
+        {
+            Naam = naam;
+            Telefoonnr = telefoonnr;
+            Groep = groep;
+            Foto = foto;
+        }
 
     }
 }
