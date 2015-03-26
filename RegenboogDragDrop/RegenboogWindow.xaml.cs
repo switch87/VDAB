7023eb1eca406d003c5676c6db1312202b7c1bb6
Gert Pellin <gert@gepe-biljarts.be>
2015-03-26 12:16:58 +0100

    11.1.1Drag-fase

--- RegenboogDragDrop/RegenboogWindow.xaml.cs
+++ RegenboogDragDrop/RegenboogWindow.xaml.cs
@@ -36,6 +36,34 @@ namespace RegenboogDragDrop
                 DragDrop.DoDragDrop(sleeprechthoek, sleepKleur, DragDropEffects.Move);
             }
         }
+
+        private void Rectangle_DragEnter(object sender, DragEventArgs e)
+        {
+            Rectangle kader = (Rectangle)sender;
+            kader.StrokeThickness = 5;
+        }
+
+        private void Rectangle_DragLeave(object sender, DragEventArgs e)
+        {
+            Rectangle kader = (Rectangle)sender;
+            kader.StrokeThickness = 3;
+        }
+
+        private void Rectangle_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent("deKleur"))
+            {
+                //Brush gesleepteKleur = (Brush)e.Data.GetData("deKleur");
+                Rectangle rechthoek = (Rectangle)sender;
+                if (rechthoek.Fill == Brushes.White)
+                {
+                    //rechthoek.Fill = gesleepteKleur;
+                    rechthoek.Fill = sleeprechthoek.Fill;
+                    sleeprechthoek.Fill = Brushes.White;
+                }
+                rechthoek.StrokeThickness = 3;
+            }
+        }
     }
 }
 
