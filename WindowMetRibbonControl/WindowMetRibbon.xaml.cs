9d6239a8acc6f0e3d4bc61e7f4dd37c5150ca120
Gert Pellin <gert@gepe-biljarts.be>
2015-04-01 13:43:53 +0200

    12.1.5 AuxiliaryPaneContent

--- WindowMetRibbonControl/WindowMetRibbon.xaml.cs
+++ WindowMetRibbonControl/WindowMetRibbon.xaml.cs
@@ -109,6 +109,55 @@ namespace WindowMetRibbonControl
         {
             MessageBox.Show("Dit is helpscherm", "Help", MessageBoxButton.OK, MessageBoxImage.Information, MessageBoxResult.OK);
         }
+
+        private void MRUGallery_SelectionChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            LeesBestand(MRUGallery.SelectedValue.ToString());
+        }
+
+        private void Radio_Click(object sender, RoutedEventArgs e)
+        {
+            RibbonRadioButton keuze = (RibbonRadioButton)sender;
+            BrushConverter bc = new BrushConverter();
+            SolidColorBrush kleur =
+            (SolidColorBrush)bc.ConvertFromString(keuze.Tag.ToString());    
+            TextBoxVoorbeeld.Foreground = kleur;
+        }
+
+        
+
+    }
+    public class BooleanToFontWeight : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((Boolean)value)
+                return "Bold";
+            else
+                return "Normal";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+
+    public class BooleanToFontStyle : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter,
+        CultureInfo culture)
+        {
+            if ((Boolean)value)
+                return "Italic";
+            else
+                return "Normal";
+        }
+        public object ConvertBack(object value, Type targetType, object parameter,
+        CultureInfo culture)
+        {
+            return null;
+        }
     }
 }
 
