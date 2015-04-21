fe84ef1d2be2ea186d7e15dd72fbb8f9d0caa42f
Gert Pellin <gert@gepe-biljarts.be>
2015-04-21 12:28:41 +0200

    ADO 4.5.3 De oefendatabase Bank

---
+++ ADONET/AdoCursus/AdoGemeenschap/BankDbManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.Common;
+
+namespace AdoGemeenschap
+{
+    class BankDbManager
+    {
+        private static ConnectionStringSettings conBankSetting =
+            ConfigurationManager.ConnectionStrings["Bank"];
+        private static DbProviderFactory factory =
+            DbProviderFactories.GetFactory(conBankSetting.ProviderName);
+
+        public DbConnection GetConnection()
+        {
+            var conBank = factory.CreateConnection();
+            conBank.ConnectionString = conBankSetting.ConnectionString;
+            return conBank;
+        }
+    }
+}
