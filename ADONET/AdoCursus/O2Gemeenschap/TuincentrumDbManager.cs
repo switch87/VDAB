e69e58646d2fec2501bfc65419431e4ae311b88a
Gert Pellin <gert@gepe-biljarts.be>
2015-04-21 13:39:21 +0200

    ADO OPGAVE 2: CONNECTIES TESTEN

---
+++ ADONET/AdoCursus/O2Gemeenschap/TuincentrumDbManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.Common;
+
+namespace O2Gemeenschap
+{
+    public class TuincentrumDbManager
+    {
+        private static ConnectionStringSettings conTuincentrumSetting =
+            ConfigurationManager.ConnectionStrings["Tuincentrum"];
+        private static DbProviderFactory factory =
+            DbProviderFactories.GetFactory( conTuincentrumSetting.ProviderName );
+
+        public DbConnection GetConnection()
+        {
+            var conTuincentrum = factory.CreateConnection();
+            conTuincentrum.ConnectionString = conTuincentrumSetting.ConnectionString;
+            return conTuincentrum;
+        }
+    }
+}
---
+++ ADONET/AdoCursus/WpfOpgave2/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+using System.Data.Common;
+using O2Gemeenschap;
+
+namespace WpfOpgave2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click( object sender, RoutedEventArgs e )
+        {
+            try
+            {
+                var manager = new TuincentrumDbManager();
+                using ( var conTuinCentrum = manager.GetConnection() )
+                {
+                    conTuinCentrum.Open();
+                    LabelStatus.Content = "Tuincentrum geopend";
+                }
+            }
+            catch ( Exception ex )
+            {
+                LabelStatus.Content = ex.Message;
+            }
+        }
+    }
+}
