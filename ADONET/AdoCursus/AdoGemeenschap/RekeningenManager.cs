aa444fe3cff3a9bcc0aa625a2e5e898812b21046
Gert Pellin <gert@gepe-biljarts.be>
2015-04-21 15:22:19 +0200

    ADO 5.3 Een SQL-statement met parameters (veranderlijke waarden).

---
+++ ADONET/AdoCursus/AdoGemeenschap/RekeningenManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Common;
+
+namespace AdoGemeenschap
+{
+    public class RekeningenManager
+    {
+        public Int32 SaldoBonus()
+        {
+            var dbManager = new BankDbManager();
+            using ( var conBank = dbManager.GetConnection() )
+            {
+                using ( var comBonus = conBank.CreateCommand() )
+                {
+                    comBonus.CommandType = System.Data.CommandType.Text;
+                    comBonus.CommandText = "update Rekeningen set Saldo=Saldo*1.1";
+                    conBank.Open();
+                    return comBonus.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public Boolean Storten( Decimal teStorten, String rekeningNr )
+        {
+            var dbManager = new BankDbManager();
+            using ( var conBank = dbManager.GetConnection() )
+            {
+                using ( var comStorten = conBank.CreateCommand() )
+                {
+                    comStorten.CommandType = System.Data.CommandType.Text;
+                    comStorten.CommandText = "update Rekeningen set Saldo=Saldo+@teStorten where RekeningNr=@RekeningNr";
+                    DbParameter parTeStorten = comStorten.CreateParameter();
+                    parTeStorten.ParameterName = "@teStorten";
+                    parTeStorten.Value = teStorten;
+                    comStorten.Parameters.Add( parTeStorten );
+                    DbParameter parRekeningNr = comStorten.CreateParameter();
+                    parRekeningNr.ParameterName = "@RekeningNr";
+                    parRekeningNr.Value = rekeningNr;
+                    comStorten.Parameters.Add( parRekeningNr );
+                    conBank.Open();
+                    return comStorten.ExecuteNonQuery() != 0;
+                }
+            }
+        }
+    }
+}
--- ADONET/AdoCursus/AdoWPF/MainWindow.xaml.cs
+++ ADONET/AdoCursus/AdoWPF/MainWindow.xaml.cs
@@ -45,5 +45,47 @@ namespace AdoWPF
                 labelStatus.Content = ex.Message;
             }
         }
+
+
+        private void buttonBonus_Click( object sender, RoutedEventArgs e )
+        {
+            try
+            {
+                var manager = new RekeningenManager();
+                labelStatus.Content = manager.SaldoBonus() + " rekeningen aangepast";
+            }
+            catch ( Exception ex )
+            {
+                labelStatus.Content = ex.Message;
+            }
+        }
+
+        private void buttonStorten_Click( object sender, RoutedEventArgs e )
+        {
+            Decimal teStorten;
+            if (decimal.TryParse(textBoxTeStorten.Text, out teStorten))
+            {
+                try
+                {
+                    var manager = new RekeningenManager();
+                    if (manager.Storten(teStorten,textBoxRekeningNr.Text))
+                    {
+                        labelStatus.Content = "OK";
+                    }
+                    else
+                    {
+                        labelStatus.Content = "Rekening niet gevonden";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    labelStatus.Content = ex.Message;
+                }
+            }
+            else
+            {
+                labelStatus.Content = "Tik een getal bij het storten";
+            }
+        }
     }
 }
