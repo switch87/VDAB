543f3e8274db62db9cdf43b32badba0ec24b7c41
Gert Pellin <gert@gepe-biljarts.be>
2015-04-22 13:29:38 +0200

    6.3 De class DbTransaction

--- ADONET/AdoCursus/AdoGemeenschap/RekeningenManager.cs
+++ ADONET/AdoCursus/AdoGemeenschap/RekeningenManager.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.Common;
+using System.Data;
 
 namespace AdoGemeenschap
 {
@@ -49,6 +50,62 @@ namespace AdoGemeenschap
                     comStorten.Parameters.Add( parRekeningNr );
                     conBank.Open();
                     return comStorten.ExecuteNonQuery() != 0;
+                }
+            }
+        }
+
+        public void Overschrijven( Decimal bedrag, string vanRekening, string naarRekening )
+        {
+            var dbManager = new BankDbManager();
+            using ( var conBank = dbManager.GetConnection() )
+            {
+                conBank.Open();
+                using ( var traOverschrijven = conBank.BeginTransaction( IsolationLevel.ReadCommitted ) )
+                {
+                    using ( var comAftrekken = conBank.CreateCommand() )
+                    {
+                        comAftrekken.Transaction = traOverschrijven;
+                        comAftrekken.CommandType = CommandType.Text;
+                        comAftrekken.CommandText = "update Rekeningen set Saldo=Saldo-@bedrag where RekeningNr=@reknr";
+
+                        var parBedrag = comAftrekken.CreateParameter();
+                        parBedrag.ParameterName = "@bedrag";
+                        parBedrag.Value = bedrag;
+                        comAftrekken.Parameters.Add( parBedrag );
+
+                        var parRekNr = comAftrekken.CreateParameter();
+                        parRekNr.ParameterName = "@reknr";
+                        parRekNr.Value = vanRekening;
+                        comAftrekken.Parameters.Add( parRekNr );
+
+                        if ( comAftrekken.ExecuteNonQuery() == 0 )
+                        {
+                            traOverschrijven.Rollback();
+                            throw new Exception( "Van rekeing bestaat niet" );
+                        }
+                    }
+                    using ( var comBijtellen = conBank.CreateCommand() )
+                    {
+                        comBijtellen.Transaction = traOverschrijven;
+                        comBijtellen.CommandType = CommandType.Text;
+                        comBijtellen.CommandText = "update rekeningen set Saldo=Saldo+@bedrag where rekeningNr=@reknr";
+
+                        var parBedrag = comBijtellen.CreateParameter();
+                        parBedrag.ParameterName = "@bedrag";
+                        parBedrag.Value = bedrag;
+                        comBijtellen.Parameters.Add( parBedrag );
+
+                        var parRekNr = comBijtellen.CreateParameter();
+                        parRekNr.ParameterName = "@reknr";
+                        parRekNr.Value = naarRekening;
+                        comBijtellen.Parameters.Add( parRekNr );
+
+                        if ( comBijtellen.ExecuteNonQuery() == 0 )
+                        {
+                            traOverschrijven.Rollback();
+                            throw new Exception( "Naar rekening bestaat niet" );
+                        }
+                    }
                 }
             }
         }
---
+++ ADONET/AdoCursus/AdoWPF/Overschrijven.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using AdoGemeenschap;
+
+namespace AdoWPF
+{
+    /// <summary>
+    /// Interaction logic for Overschrijven.xaml
+    /// </summary>
+    public partial class Overschrijven : Window
+    {
+        public Overschrijven()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOverschrijven_Click( object sender, RoutedEventArgs e )
+        {
+            Decimal bedrag;
+            if(Decimal.TryParse(textBoxBedrag.Text,out bedrag))
+            {
+                try
+                {
+                    var manager = new RekeningenManager();
+                    manager.Overschrijven( bedrag, textBoxNaarRekNr.Text, textBoxNaarRekNr.Text );
+                    labelStatus.Content = "OK";
+                }
+                catch (Exception ex)
+                {
+                    labelStatus.Content = ex.Message;
+                }
+            }
+            else
+            {
+                labelStatus.Content = "bedrag bevat geen getal";
+            }
+        }
+    }
+}
