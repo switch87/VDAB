f3ab07f9eb8974ffff026ec9f66a62615dab969b
Gert Pellin <gert@gepe-biljarts.be>
2015-04-22 15:19:03 +0200

    database-procedure

---
+++ ADONET/Bestanden/dbo.leverancierToevoegen.sql
@@ -0,0 +1,5 @@
+﻿CREATE PROCEDURE leverancierToevoegen
+	(@naam nvarchar(30), @adres nvarchar(30), @postnr nvarchar(10), @woonplaats nvarchar(30))
+AS
+	insert into Leveranciers (Naam, Adres, PostNr, Woonplaats)
+	values (@naam, @adres, @postNr, @woonplaats)
