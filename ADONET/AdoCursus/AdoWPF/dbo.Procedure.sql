ea2740ab1e5f603fa49fddedf5205473329b6b04
Gert Pellin <gert@gepe-biljarts.be>
2015-04-22 09:25:57 +0200

    5.4.3 Stored procedure aanroepen / procedure file

---
+++ ADONET/AdoCursus/AdoWPF/dbo.Procedure.sql
@@ -0,0 +1,5 @@
+﻿CREATE PROCEDURE storten (@teStorten money, @rekeningNr nvarchar(14))
+AS
+	UPDATE Rekeningen
+	SET Saldo=Saldo+@teStorten
+	WHERE RekeningNr=@rekeningNr
---
+++ ADONET/Bestanden/dbo.Storten.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE Storten
+	(@teStorten money, @rekeningNr nvarchar(14))
+AS
+	update Rekeningen
+	set saldo=saldo+@teStorten
+	where rekeningnr=@rekeningNr
