d8245d05876a0c719e053011c3c581feab6b4888
Gert Pellin <gert@gepe-biljarts.be>
2015-04-29 15:37:50 +0200

    ADO: Opgave 6: SQL-Procedure

---
+++ ADONET/Bestanden/dbo.Opgave6.PlantInfoOpvragen.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE plantInfoOpvragen
+	@plantNr int,
+	@naam nvarchar(50) output,
+	@soort nvarchar(50) output,
+	@leverancier nvarchar(50) output,
+	@kleur nvarchar(50) output,
+	@kostprijs money output
+
+AS
+	SELECT @naam=Planten.Naam, @soort=Soorten.Soort, @leverancier=Leveranciers.Naam, @kleur=Planten.kleur, @kostprijs=Planten.VerkoopPrijs
+	from (planten inner join soorten on Planten.SoortNr = Soorten.SoortNr) inner join Leveranciers on Planten.Levnr = Leveranciers.LevNr
+	where @plantNr=Planten.PlantNr
