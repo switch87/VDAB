734302227338cd71b41053187b605055e7783a66
Gert Pellin <gert@gepe-biljarts.be>
2015-06-09 11:15:34 +0200

    13.6

--- ASP.NET/MVC_Voorbeeld1/MVC_Voorbeeld3/Models/Persoon.cs
+++ ASP.NET/MVC_Voorbeeld1/MVC_Voorbeeld3/Models/Persoon.cs
@@ -10,14 +10,23 @@ namespace MVC_Voorbeeld3.Models
     {
         public int ID { get; set; }
         public string Voornaam { get; set; }
+        [StringLength( 255, ErrorMessage = "Max. {1} tekens voor {0}" )]
         public string Familienaam { get; set; }
         public int Score { get; set; }
         [DisplayFormat( DataFormatString = "{0:#,##0.00}" )]
         public decimal Wedde { get; set; }
+        [DataType( DataType.Password )]
+        [StringLength( 20, MinimumLength = 8, ErrorMessage = "Het paswoord bevat min. {2}, max. {1} tekens" )]
         public string Paswoord { get; set; }
-        [DisplayFormat( DataFormatString = "{0:d}" )]
+        [DataType( DataType.Password )]
+        [Compare( "Paswoord", ErrorMessage = "{0} verschilt van {1}" )]
+        public string HerhaalPaswoord { get; set; }
+        [DisplayFormat( DataFormatString = "{0:d}", ApplyFormatInEditMode = true )]
+        [DataType( DataType.Date )]
+        [Verleden( ErrorMessage = "Geboortedatum moet in het verleden liggen" )]
         public DateTime Geboren { get; set; }
         public bool Gehuwd { get; set; }
+        [DataType( DataType.MultilineText )]
         public string Opmerkingen { get; set; }
         public Geslacht Geslacht { get; set; }
     }
---
+++ ASP.NET/MVC_Voorbeeld1/MVC_Voorbeeld3/Models/VerledenAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace MVC_Voorbeeld3.Models
+{
+    class VerledenAttribute : ValidationAttribute
+    {
+        public override bool IsValid( object value )
+        {
+            if ( value == null )
+            {
+                return true;
+            }
+            if ( !( value is DateTime ) )
+            {
+                return false;
+            }
+            return ( (DateTime)value ) < DateTime.Today;
+        }
+    }
+}
