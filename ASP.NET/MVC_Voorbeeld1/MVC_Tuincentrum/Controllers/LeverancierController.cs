5b83171b381e3ccf1b2700fcd7226cbd628527ac
Gert Pellin <gert@gepe-biljarts.be>
2015-06-09 13:15:38 +0200

    14.2 Scaffolding

---
+++ ASP.NET/MVC_Voorbeeld1/MVC_Tuincentrum/Controllers/LeverancierController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Tuincentrum.Models;
+
+namespace MVC_Tuincentrum.Controllers
+{
+    public class LeverancierController : Controller
+    {
+        private MVCTuinCentrumEntities db = new MVCTuinCentrumEntities();
+
+        // GET: Leverancier
+        public ActionResult Index()
+        {
+            return View(db.Leveranciers.ToList());
+        }
+
+        // GET: Leverancier/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Leverancier leverancier = db.Leveranciers.Find(id);
+            if (leverancier == null)
+            {
+                return HttpNotFound();
+            }
+            return View(leverancier);
+        }
+
+        // GET: Leverancier/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Leverancier/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "LevNr,Naam,Adres,PostNr,Woonplaats")] Leverancier leverancier)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Leveranciers.Add(leverancier);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(leverancier);
+        }
+
+        // GET: Leverancier/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Leverancier leverancier = db.Leveranciers.Find(id);
+            if (leverancier == null)
+            {
+                return HttpNotFound();
+            }
+            return View(leverancier);
+        }
+
+        // POST: Leverancier/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "LevNr,Naam,Adres,PostNr,Woonplaats")] Leverancier leverancier)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(leverancier).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(leverancier);
+        }
+
+        // GET: Leverancier/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Leverancier leverancier = db.Leveranciers.Find(id);
+            if (leverancier == null)
+            {
+                return HttpNotFound();
+            }
+            return View(leverancier);
+        }
+
+        // POST: Leverancier/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Leverancier leverancier = db.Leveranciers.Find(id);
+            db.Leveranciers.Remove(leverancier);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ASP.NET/MVC_Voorbeeld1/MVC_Tuincentrum/Controllers/PlantController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Tuincentrum.Models;
+
+namespace MVC_Tuincentrum.Controllers
+{
+    public class PlantController : Controller
+    {
+        private MVCTuinCentrumEntities db = new MVCTuinCentrumEntities();
+
+        // GET: Plant
+        public ActionResult Index()
+        {
+            var planten = db.Planten.Include(p => p.Leveranciers).Include(p => p.Soorten);
+            return View(planten.ToList());
+        }
+
+        // GET: Plant/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Plant plant = db.Planten.Find(id);
+            if (plant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(plant);
+        }
+
+        // GET: Plant/Create
+        public ActionResult Create()
+        {
+            ViewBag.Levnr = new SelectList(db.Leveranciers, "LevNr", "Naam");
+            ViewBag.SoortNr = new SelectList(db.Soorten, "SoortNr", "Naam");
+            return View();
+        }
+
+        // POST: Plant/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PlantNr,Naam,SoortNr,Levnr,Kleur,VerkoopPrijs")] Plant plant)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Planten.Add(plant);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Levnr = new SelectList(db.Leveranciers, "LevNr", "Naam", plant.Levnr);
+            ViewBag.SoortNr = new SelectList(db.Soorten, "SoortNr", "Naam", plant.SoortNr);
+            return View(plant);
+        }
+
+        // GET: Plant/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Plant plant = db.Planten.Find(id);
+            if (plant == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Levnr = new SelectList(db.Leveranciers, "LevNr", "Naam", plant.Levnr);
+            ViewBag.SoortNr = new SelectList(db.Soorten, "SoortNr", "Naam", plant.SoortNr);
+            return View(plant);
+        }
+
+        // POST: Plant/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PlantNr,Naam,SoortNr,Levnr,Kleur,VerkoopPrijs")] Plant plant)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(plant).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Levnr = new SelectList(db.Leveranciers, "LevNr", "Naam", plant.Levnr);
+            ViewBag.SoortNr = new SelectList(db.Soorten, "SoortNr", "Naam", plant.SoortNr);
+            return View(plant);
+        }
+
+        // GET: Plant/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Plant plant = db.Planten.Find(id);
+            if (plant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(plant);
+        }
+
+        // POST: Plant/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Plant plant = db.Planten.Find(id);
+            db.Planten.Remove(plant);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
