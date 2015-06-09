e1662e48fb2c109454090af89ef5ceb46e0f015d
Gert Pellin <gert@gepe-biljarts.be>
2015-06-09 12:24:49 +0200

    14.1 De database voorbereiden

---
+++ ASP.NET/MVC_Voorbeeld1/MVC_Tuincentrum/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_Tuincentrum.Startup))]
+namespace MVC_Tuincentrum
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
