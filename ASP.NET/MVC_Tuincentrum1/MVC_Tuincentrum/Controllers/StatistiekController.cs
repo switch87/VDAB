6adc4042e700fd5b1d28d5ada8c62f4369062add
Gert Pellin <gert@gepe-biljarts.be>
2015-06-11 11:24:16 +0200

    17 ActionFilters 17.1 ActionFilters voor alle controllers

---
+++ ASP.NET/MVC_Tuincentrum1/MVC_Tuincentrum/Controllers/StatistiekController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Tuincentrum.Controllers
+{
+    public class StatistiekController : Controller
+    {
+        // GET: Statistiek
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ASP.NET/MVC_Tuincentrum1/MVC_Tuincentrum/Filters/StatistiekActionFilter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace MVC_Tuincentrum.Filters
+{
+    public class StatistiekActionFilter : ActionFilterAttribute
+    {
+        private static readonly Dictionary<string, int> _statistiek = new Dictionary<string, int>();
+
+        public static Dictionary<string, int> Statistiek
+        {
+            get { return _statistiek; }
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var url = filterContext.HttpContext.Request.Url.ToString();
+            lock (_statistiek)
+            {
+                if (_statistiek.ContainsKey(url))
+
+                    _statistiek[url]++;
+                else
+                    _statistiek[url] = 1;
+            }
+        }
+    }
+}
