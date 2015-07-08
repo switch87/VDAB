768059240d0fb1d83bfcff3dbc682b2bef961910
switch87 <gert@gepe-biljarts.be>
2015-07-08 13:20:22 +0200

    1.2 Planten service host

---
+++ wcf/BierenServiceLibrary/ConsoleApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using BierenServiceLibrary;
+
+namespace ConsoleBierenServiceHost
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            using (var serviceHost = new ServiceHost(typeof (BierenService)))
+            {
+                serviceHost.Open();
+                Console.WriteLine("Druk s om de service te stoppen");
+                while (Console.ReadLine()!="s")
+                {
+                }
+            }
+        }
+    }
+}
